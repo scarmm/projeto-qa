92fe1a7e5b5940d21073830c972377f101ae4633
SARA RUBIA DO CARMO <sara.carmo@alunos.sc.senac.br>
2025-03-17 22:12:34 -0300

    alterações front-end

--- register.php
+++ register.php
@@ -21,25 +21,12 @@
         <main class="container">
 
             <section class="panel-section">
-                <div>
-                    <h1>Registrar</h1>
-                </div>
-
-                <?php if (isset($_GET["typeMsg"]) && isset($_GET["message"])){ ?>
-                    <?php if ($_GET["typeMsg"] == "success"){ ?>
-                        <div class="success information">
-                            <span><?php echo htmlspecialchars($_GET["message"], ENT_QUOTES, 'UTF-8'); ?></span>
-                        </div>
-                    <?php }else if ($_GET["typeMsg"] == "error"){ ?>
-                        <div class="error information">
-                            <span><?php echo htmlspecialchars($_GET["message"], ENT_QUOTES, 'UTF-8'); ?></span>
-                        </div>
-                    <?php }; ?>
-                <?php }; ?>
-
                 <form action="src/php/global.php" method="POST">
 
-                    <!-- protection -->
+                   <!-- protection -->
+                    
+                    <h1>Registrar</h1>
+
                     <input type="hidden" name="type" value="register">
                     <input type="hidden" name="csrf_token" value="LSZzKRtj2Ke">
 
@@ -52,6 +39,19 @@
                     <div class="form-group">
                         <input type="password" name="senhaC" placeholder="Confirmar Senha">
                     </div>
+
+                    <?php if (isset($_GET["typeMsg"]) && isset($_GET["message"])){ ?>
+                    <?php if ($_GET["typeMsg"] == "success"){ ?>
+                        <div class="success information">
+                            <span><?php echo htmlspecialchars($_GET["message"], ENT_QUOTES, 'UTF-8'); ?></span>
+                        </div>
+                    <?php }else if ($_GET["typeMsg"] == "error"){ ?>
+                        <div class="error information">
+                            <span><?php echo htmlspecialchars($_GET["message"], ENT_QUOTES, 'UTF-8'); ?></span>
+                        </div>
+                    <?php }; ?>
+                    <?php }; ?>
+
                     <div class="form-group">
                         <button type="submit">Confirmar</button>
                     </div>
